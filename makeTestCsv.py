ba3c445f06ad460cc3faa53e2b87fc2ec8ec0180
CodeJin19 <codejin19@gmail.com>
2020-11-07 20:57:47 +0900

    made exception for teams

--- makeTestCsv.py
+++ makeTestCsv.py
@@ -14,6 +14,38 @@ def controler(f):
 
     wr = csv.writer(f)
 
+    teamList = ['OB', 'LT', 'SS', 'HH', 'HD', 'HT', 'LG', 'SK', 'WO', 'NC', 'KT']
+    teamKey = ['두산', '롯데', '삼성', '한화', '현대', 'KIA', 'LG', 'SK', '넥센', 'NC', 'KT']
+
+    for year in range(yearFrom, (yearTo + 1)):
+        if year < 2008:
+            for i in range(len(teamList)):
+                for j in range(len(teamList)):
+                    if i == j or teamList[i] == 'WO' or teamList[i] == 'NC' or teamList[i] == 'KT' or teamList[j] == 'WO' or teamList[j] == 'NC' or teamList[j] == 'KT':
+                        continue
+                    else:
+                        conn
+        elif year < 2013:
+            for i in range(len(teamList)):
+                for j in range(len(teamList)):
+                    if i == j or teamList[i] == 'HD' or teamList[i] == 'NC' or teamList[i] == 'KT' or teamList[j] == 'HD' or teamList[j] == 'NC' or teamList[j] == 'KT':
+                        continue
+                    else:
+                        conn
+        elif year < 2015:
+            for i in range(len(teamList)):
+                for j in range(len(teamList)):
+                    if i == j or teamList[i] == 'HD' or teamList[i] == 'KT' or teamList[j] == 'HD' or teamList[j] == 'KT':
+                        continue
+                    else:
+                        conn
+        else:
+            for i in range(len(teamList)):
+                for j in range(len(teamList)):
+                    if i == j or teamList[i] == 'HD' or teamList[j] == 'HD':
+                        continue
+                    else:
+                        conn
 
 
 """
